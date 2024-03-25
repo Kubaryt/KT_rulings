c026924f3b43a93d05a8450e1e78f4f30f091b94
Kubaryt <88268063+Kubaryt@users.noreply.github.com>
2024-03-25 17:24:10 +0100

    feat: Tailwind support! (#1)

---
+++ tailwind.coinfig.js
@@ -0,0 +1,10 @@
+// Not used in code! Only for PyCharm to recognize Tailwind usage
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
